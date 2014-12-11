50b4743aa694bbe521f4196b4141b92a260eff82
datawhore <code4chaosmobile@gmail.com>
2014-12-11 07:45:59 -0600

    added base files

---
+++ main.js
@@ -0,0 +1,8 @@
+// var toastGroupTemplate = document.querySelector('#toastGroup');
+// toastGroupTemplate.showToast = function() {
+//   document.querySelector('#toast').show();
+// }
+
+onload = function() {
+  console.log('onLoad()..');
+}
